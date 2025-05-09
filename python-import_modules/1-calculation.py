a67b889bf54fcd1ff008d653b60aed2ae31c2a30
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-09 14:53:20 +0200

    alias calculette

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import calculator_1
+import calculator_1 as calculette
 a = 10
 b = 5
-print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
-print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
-print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
-print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
+print("{} + {} = {}".format(a, b, calculette.add(a, b)))
+print("{} - {} = {}".format(a, b, calculette.sub(a, b)))
+print("{} * {} = {}".format(a, b, calculette.mul(a, b)))
+print("{} / {} = {}".format(a, b, calculette.div(a, b)))
