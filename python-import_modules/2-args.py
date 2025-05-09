49791f9662b453be659bf1fe0af07e21fb5cead7
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-09 15:52:55 +0200

    Protect code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-args = sys.argv[1:]
-if len(args) == 0:
-    print("0 arguments.")
-elif len(args) > 0:
-    print("{} arguments:".format(len(args)))
+if __name__ == "__main__":
+    import sys
+    args = sys.argv[1:]
+    if len(args) == 0:
+        print("0 arguments.")
+    elif len(args) > 0:
+        print("{} arguments:".format(len(args)))
 
-for i, arg in enumerate(args):
-    print("{}: {}".format(i + 1, arg))
+    for i, arg in enumerate(args):
+        print("{}: {}".format(i + 1, arg))
