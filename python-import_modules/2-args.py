a8354a1ca79cb7ff5d76c5b35cd7844bc39f49a8
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-09 16:02:20 +0200

    without s if only one parameter

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,12 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     if len(args) == 0:
         print("0 arguments.")
-    elif len(args) > 0:
+    elif len(args) == 1:
+        print("{} argument:".format(len(args)))
+        for i, arg in enumerate(args):
+            print("{}: {}".format(i + 1, arg))
+    elif len(args) > 1:
         print("{} arguments:".format(len(args)))
         for i, arg in enumerate(args):
             print("{}: {}".format(i + 1, arg))
+
