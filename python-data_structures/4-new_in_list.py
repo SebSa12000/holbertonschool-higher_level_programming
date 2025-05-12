16e0f0b89d4423ede21ebdb4ab09924c76b6d2ef
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-12 13:42:31 +0200

    Task 4-5

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+def new_in_list(my_list, idx, element):
+    newlist = list(my_list)
+    if idx < 0:
+        return None
+    if idx >= len(newlist):
+        return None
+    newlist[idx] = element
+    return newlist
