92a432532e23f76c32630f308648bffcabb760d0
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-13 11:19:52 +0200

    pycode style

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     return_elem = []
-    for v1 in set_1: 
+    for v1 in set_1:
         trouve = 0
         for v2 in set_2:
-            if v1 == v2: 
+            if v1 == v2:
                 trouve = 1
         if trouve == 0:
             return_elem.append(v1)
-    for v1 in set_2: 
+    for v1 in set_2:
         trouve = 0
         for v2 in set_1:
-            if v1 == v2: 
+            if v1 == v2:
                 trouve = 1
         if trouve == 0:
             return_elem.append(v1)
