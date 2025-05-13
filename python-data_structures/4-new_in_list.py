f24a9972e800e1776feba75dc9e8271bbf090b56
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-13 09:41:11 +0200

    new in list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    newl = list(my_list)
     if idx < 0:
-        return None
+        return newl
     if idx >= len(my_list):
-        return None
-    my_list[idx] = element
-    return my_list
+        return newl
+    newl[idx] = element
+    return newl
