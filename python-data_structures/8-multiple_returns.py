364c4811b27664c03139067c8eb0410fc0cdc330
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-13 10:22:32 +0200

    exec + len

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     """Returns the length of a string and its first character."""
-    if sentence == "":
+    if len(sentence) == 0:
         return 0, None
     else:
         return len(sentence), sentence[0]
-    
