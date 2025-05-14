9620519122aa98f52ef9c6c8e7252d1409182131
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-14 14:41:54 +0200

    pycodestyle

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,11 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     compteur = 0
     for val in my_list:
-        try:
-            if compteur < x:
+        if compteur < x:
+            try:
                 print(val, end="")
                 compteur = compteur + 1
-        except:
-            print("", end="")
+            except BaseException:
+                print("", end="")
     print("")
     return compteur
