fc18dd91134da3c2979cf63d756676991c82632f
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-14 10:56:34 +0200

    test string

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,22 +2,23 @@
 def roman_to_int(roman_string):
     sum = 0
     precedent = ''
-    for character in roman_string:
-        if character == 'I':
-            sum = sum + 1
-        elif character == 'V':
-            if precedent == 'I':
-                sum = sum - 2
-            sum = sum + 5
-        elif character == 'X':
-            if precedent == 'I':
-                sum = sum - 2
-            sum = sum + 10
-        elif character == 'L':
-            sum = sum + 50
-        elif character == 'C':
-            sum = sum + 100
-        elif character == 'D': 
-            sum = sum + 500
-        precedent = character
+    if isinstance(roman_string, str):
+        for character in roman_string:
+            if character == 'I':
+                sum = sum + 1
+            elif character == 'V':
+                if precedent == 'I':
+                    sum = sum - 2
+                sum = sum + 5
+            elif character == 'X':
+                if precedent == 'I':
+                    sum = sum - 2
+                sum = sum + 10
+            elif character == 'L':
+                sum = sum + 50
+            elif character == 'C':
+                sum = sum + 100
+            elif character == 'D': 
+                sum = sum + 500
+            precedent = character
     return sum
