8bcffcde4e77187502ce19a38883ac74d057e82e
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-15 16:38:02 +0200

    pycodestyle

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    #division by zero
+    # division by zero
     try:
         result = a / b
     except ZeroDivisionError:
         result = None
     finally:
-        # if no error 
+        # if no error
         print("Inside result: {}".format(result))
     return result
