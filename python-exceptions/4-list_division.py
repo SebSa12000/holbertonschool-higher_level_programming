7561d4f04d0713ead3b118ae68c3cacff6472535
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-15 18:58:36 +0200

    task 3 et 4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    """Divides two lists element by element.
+
+    Args:
+        my_list_1 (list): The first list.
+        my_list_2 (list): The second list.
+        list_length (int): The number of elements to divide.
+
+    Returns:
+        list: A new list with the results of the division.
+    """
+    result = []
+    for i in range(list_length):
+        try:
+            result.append(my_list_1[i] / my_list_2[i])
+        except TypeError:
+            print("wrong type")
+            result.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
+        except IndexError:
+            print("out of range")
+            result.append(0)
+        finally:
+            pass
+    return result
