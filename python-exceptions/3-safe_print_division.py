62d2f728d33925ea04c5b3df20466408c40cc4f4
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-15 16:36:53 +0200

    check divide by zero

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    #division by zero
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        # if no error 
+        print("Inside result: {}".format(result))
+    return result
