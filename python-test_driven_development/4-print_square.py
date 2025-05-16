ea33de7892d3f757cba3d7f21a75e56517847af7
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-16 15:29:41 +0200

    refactor: improve input validation for print_square function

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -6,8 +6,15 @@ Return: None
 
 
 def print_square(size):
+    if not isinstance(size, (float)) and size < 0:
+        raise TypeError("size must be an integer")
+    if not isinstance(size, (int)):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
     for i in range(size):
         for j in range(size):
-            print("#", end ="")
+            print("#", end="")
         print("")
     print("")
