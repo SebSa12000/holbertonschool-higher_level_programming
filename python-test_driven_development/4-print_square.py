20c99f4c323f03a5ebe753b6efabedd58c53a74e
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-16 16:23:10 +0200

    check test

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 '''
 Function that display a square
-Return: None
 '''
 
 
 def print_square(size):
-    if not isinstance(size, (float)) and size < 0:
-        raise TypeError("size must be an integer")
     if not isinstance(size, (int)):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-
+    
     for i in range(size):
         for j in range(size):
             print("#", end="")
         print("")
-
