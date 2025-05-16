297c878752d9189fb6b6ef33a69efc7faf116512
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-16 16:59:44 +0200

    less caracter

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -8,8 +8,8 @@ def text_indentation(text):
     if not isinstance(text, (str)):
         raise TypeError("text must be a string")
     for i in text:
+        print(i, end="")
         if i == '.' or i == ':' or i == '?':
             print("")
             print("")
-        else:
-            print(i, end="")
+            
