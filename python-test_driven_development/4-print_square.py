32fab4a2238e7d6e7a0f9c068c33faa7ddc1a93f
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-16 15:21:19 +0200

    add print_square function to display a square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+Function that display a square
+Return: None
+'''
+
+
+def print_square(size):
+    for i in range(size):
+        for j in range(size):
+            print("#", end ="")
+        print("")
+    print("")
