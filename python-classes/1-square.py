5a630459df181beb9b2fc15c2cdb831469c37b32
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-19 14:12:04 +0200

    square

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+
+    def __init__(self, size):
+        __size = size
+
+    def get_size(self):
+        return self.__size
+
+    def set_size(self, x):
+        self.__size = x
