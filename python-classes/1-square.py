4802241a619e4e0a4db1f98bb8f3c2f277d90c61
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-19 14:24:13 +0200

    tesk 1 attribute

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,10 +2,5 @@
 class Square:
 
     def __init__(self, size):
-        __size = size
+        self._Square__size = size
 
-    def get_size(self):
-        return self.__size
-
-    def set_size(self, x):
-        self.__size = x
