f4920527eac7ccc0845f4a6f3f8269de947f5ba2
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-20 09:39:35 +0200

    task 2,3,4,5

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+'''
+Class Square
+'''
 class Square:
-
+    '''
+    Init the function
+    '''
     def __init__(self, size):
         self._Square__size = size
 
---
+++ python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Class Square
+'''
+class Square:
+    '''
+    Init function and raise error if it's a string or < 0 
+    '''
+    def __init__(self, size=0):
+        self._Square__size = 0
+        if  isinstance(size, str):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
+
