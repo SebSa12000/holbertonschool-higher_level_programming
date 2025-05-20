56cef21e6db7ac33a42f308152e85feecc3343ec
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-20 10:41:54 +0200

    pycodestyle

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,13 +2,16 @@
 '''
 Class Square
 '''
+
+
 class Square:
     '''
-    Init function and raise error if it's a string or < 0 
+    Init function and raise error if it's a string or < 0
     '''
     def __init__(self, size=0):
+        ''' initialize and raise error if needed '''
         self._Square__size = 0
-        if  isinstance(size, str):
+        if isinstance(size, str):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
@@ -18,5 +21,5 @@ class Square:
     Function area
     '''
     def area(self):
+        ''' return the ² of the size'''
         return self._Square__size * self._Square__size
-    
