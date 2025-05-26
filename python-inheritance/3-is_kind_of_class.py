66a60ab5ecff45b8abc3d97b682dc267ea5be4c6
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-26 15:55:57 +0200

    task 3 pycodestyle

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-''' is_kind_of_class '''
+'''
+is_kind_of_class
+'''
+
+
 def is_kind_of_class(obj, a_class):
+    ''' return if the class is an instance of a_class '''
     return isinstance(obj, a_class)
