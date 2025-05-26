b63172f8852d54804a86302f82b183f1bc046d1f
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-26 14:39:07 +0200

    task 0 , 4

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-''' liste des membre de la classes '''
+'''
+liste des membre de la classes
+'''
+
+
 def lookup(obj):
+    ''' return list of properti of the object  '''
     return dir(obj)
