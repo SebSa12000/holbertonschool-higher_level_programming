c20067f19f656c873bfb114fa1c36426d10e0d4c
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-27 15:18:58 +0200

    pycodestyle

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 '''
-    Class Square 
+    Class Square
 '''
 
 
 Rectangle = __import__('9-rectangle').Rectangle
 
-class Square(Rectangle):    
+
+class Square(Rectangle):
     ''' a square class '''
-    
+
     def __init__(self, size):
         ''' initialize the square '''
         self.integer_validator("size", size)
@@ -17,8 +18,7 @@ class Square(Rectangle):
 
     def area(self):
         return self.__size * self.__size
-    
+
     def __str__(self):
         ret = "[Square] " + str(self.__size) + "/" + str(self.__size)
         return ret
-    
