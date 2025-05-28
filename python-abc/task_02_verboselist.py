23b32719ff3ac5f908048a4c25d595a519f51cf4
SebSa12000 <sebastien.salgues@gmail.com>
2025-05-28 15:00:09 +0200

    task 2

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+    Class verboselist
+'''
+
+
+class VerboseList(list):
+    ''' surcharge append '''
+    def append(self, value):
+        print(f"Added [{value}] to the list.")
+        super().append(value)
+
+    ''' surcharge extend '''
+    def extend(self, value):
+        super().extend(value)
+        print(f"Extended the list with [{len(value)}] items.")
+
+    ''' surcharge remove '''
+    def remove(self, value):
+        print(f"Removed [{value}] from the list.")
+        super().remove(value)
+
+    ''' surcharge '''
+    def pop(self, item=-1):
+        print("Popped [{}] form the list.".format(self[item]))
+        super().pop(item)
