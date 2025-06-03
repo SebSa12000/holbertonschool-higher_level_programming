d1ba7c19463f7c5ddf894f13e9a3f3ef86d8a312
SebSa12000 <sebastien.salgues@gmail.com>
2025-06-03 15:20:53 +0200

    task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -8,7 +8,7 @@ def write_file(filename="", text=""):
     '''
         write a file
     '''
-    f = open(filename, "w", encoding="utf-8")
-    ret = f.write(text)
-    f.close()
-    return ret
+    with open(filename, "w", encoding="utf-8") as f:
+        ret = f.write(text)
+        f.close()
+        return ret
