c50f100628818d06826527d0db816c7c7ebc2509
SebSa12000 <sebastien.salgues@gmail.com>
2025-06-03 14:25:03 +0200

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,7 +8,7 @@ def read_file(filename=""):
     ''' 
     function to read a file 
     '''
-    with open(filename, "r", encoding="utf-8") as file:
+    with open(filename, encoding="utf-8") as file:
         for line in file:
             if line:
                 print(line, end="")
