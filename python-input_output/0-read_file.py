53de50b31e3f2545a1945946e0cc0e6b9b59d11f
SebSa12000 <sebastien.salgues@gmail.com>
2025-06-03 14:26:28 +0200

    pycodestyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,8 +5,8 @@
 
 
 def read_file(filename=""):
-    ''' 
-    function to read a file 
+    '''
+    function to read a file
     '''
     with open(filename, encoding="utf-8") as file:
         for line in file:
