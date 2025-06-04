d00c61ac0b40db2fdab3318ab0e768811e419969
SebSa12000 <sebastien.salgues@gmail.com>
2025-06-04 15:09:37 +0200

    fix comment pb

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+''' Adds all arguments to a python list and
+    save them to a file'''
+
+
 import sys
 import json
-'''
-    Class Rectangle
-'''
-
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
