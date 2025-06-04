feb6fcd8236c03cc465421e9a00e12a6eb01b00a
SebSa12000 <sebastien.salgues@gmail.com>
2025-06-04 10:20:15 +0200

    try again fix

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+'''Returns the JSN rep of and object(string)'''
+
+
 import json
 
-def to_json_string(my_obj):
-    '''
-        convert an object to a JSON string
-    '''
-    return json.dumps(my_obj)
+
+def to_json_string(my_ojb):
+    '''Return the jsn rep of an object(string)'''
+    return json.dumps(my_ojb)
