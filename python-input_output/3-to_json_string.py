392fd85b2d7bc59a905a3e6b4671f0b555fdf410
SebSa12000 <sebastien.salgues@gmail.com>
2025-06-04 10:14:36 +0200

    pb comment and code

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -2,7 +2,6 @@
 import json
 
 def to_json_string(my_obj):
-    '''
-        to_json_string return the dump of json
-    '''
-    return json.dumps(my_obj)
+    ''' to_json_string return the dump of json '''
+    var = json.dumps(my_obj)
+    return var
