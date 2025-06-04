595bd4e336e482230b1f7d727e58b2e8248b0424
SebSa12000 <sebastien.salgues@gmail.com>
2025-06-04 15:06:37 +0200

    task 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+import json
+'''
+    Class Rectangle
+'''
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+try:
+
+    obj = load_from_json_file("add_item.json")
+except BaseException:
+    obj = []
+
+for i in sys.argv[1:]:
+    obj.append(i)
+save_to_json_file(obj, "add_item.json")
