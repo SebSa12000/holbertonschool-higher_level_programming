3bebf665093595c278089fc04f832ddf3677df97
SebSa12000 <sebastien.salgues@gmail.com>
2025-06-23 16:45:51 +0200

    hide venv

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,3 +1,6 @@
 -- CREATE TABLE WITH VALUES
 CREATE TABLE IF NOT EXISTS second_table ( id int, name varchar(256), score int );
-INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10),
+(2, 'Alex', 3), 
+(3, 'Bob', 14), 
+(4, 'George', 8);
