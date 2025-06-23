2069af82725fc5547177010354ca360d1b4bfc9d
SebSa12000 <sebastien.salgues@gmail.com>
2025-06-23 16:09:55 +0200

    uppercase

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,3 +1,3 @@
 -- CREATE TABLE WITH VALUES
 CREATE TABLE IF NOT EXISTS second_table ( id int, name varchar(256), score int );
-INSERT INTO second_table (id, name, score) values (1, 'John', 10), (2, 'Alex', 3), (3, 'BOB', 14), (4, 'George', 8);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'BOB', 14), (4, 'George', 8);
