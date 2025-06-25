3f495f0def51665c55d9cd87824ba7927865b1b8
SebSa12000 <sebastien.salgues@gmail.com>
2025-06-25 11:47:23 +0200

    task 15 without having

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,6 @@
 -- list dexter
-SELECT tv_shows.tite, count(*) FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_genres.name = 'Comedy'
-GROUP BY tv_shows.tite
-HAVING COUNT(*) = 1;
+SELECT tv_shows.title FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
