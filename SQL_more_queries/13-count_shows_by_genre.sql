83dad1b4e8c2fa00339aaad30a87c6eb11fd1b7a
SebSa12000 <sebastien.salgues@gmail.com>
2025-06-25 10:17:45 +0200

    task 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,6 @@
 -- count genre
-SELECT tv_show_genres.title as genre, count(*) as number_of_shows
-FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-GROUP BY tv_show_genres.title
+SELECT tv_genres.name AS genre,
+COUNT(tv_show_genres.genre_id) AS number_of_shows FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
