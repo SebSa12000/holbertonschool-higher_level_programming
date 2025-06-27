0d41798d627a012b36eeba0af31b49aa43934803
SebSa12000 <sebastien.salgues@gmail.com>
2025-06-27 14:08:38 +0200

    pycodestile

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -16,7 +16,10 @@ if __name__ == '__main__':
         charset="utf8"
         )
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM states INNER JOIN cities ON cities.state_id = states.id ORDER BY cities.id ASC" )
+    cur.execute("SELECT cities.id, cities.name, states.name "
+                "FROM states "
+                "INNER JOIN cities ON cities.state_id = states.id "
+                "ORDER BY cities.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
