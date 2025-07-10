5c6405db8df6670d14c4aeed9c5c2f714be9d96b
SebSa12000 <sebastien.salgues@gmail.com>
2025-07-10 16:38:38 +0200

    task 3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
+let trouve = 0;
+process.argv.slice(2).forEach((val) => {console.log(val); trouve++;});
+if (trouve === 0)
+{
   console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found:', process.argv[2]);
-} else {
-  console.log('Arguments found:', process.argv.slice(2).join(', '));
 }
+
