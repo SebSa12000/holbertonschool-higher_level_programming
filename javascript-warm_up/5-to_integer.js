7e0a9799ec14278c5003e5e68755846393aeeb86
SebSa12000 <sebastien.salgues@gmail.com>
2025-07-11 09:16:30 +0200

    task 5 semistandard

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-function isNumeric(str) {
-  if (typeof str != "string") return false // we only process strings!  
+function isNumeric (str) {
+  if (typeof str !== 'string') return false; // we only process strings!
   return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
-         !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+         !isNaN(parseFloat(str)); // ...and ensure strings of whitespace fail
 }
 
-if (isNumeric(process.argv[2]))
-    console.log('My number: ' + process.argv[2]);
-else
-    console.log('Not a number');
-
+if (isNumeric(process.argv[2])) { console.log('My number: ' + process.argv[2]); } else { console.log('Not a number'); }
