be5c94a02f1aa64dd0063ff15937e8b6467f4f84
SebSa12000 <sebastien.salgues@gmail.com>
2025-07-11 10:38:10 +0200

    task 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
-let max1 = process.argv[2];
-let max2 = process.argv[3];
-if (process.argv[2] < process.argv[3]) {
-  max1 = process.argv[3];
-  max2 = process.argv[2];
+let max1 = parseFloat(process.argv[2]);
+let max2 = parseFloat(process.argv[3]);
+if (parseFloat(process.argv[2]) < parseFloat(process.argv[3])) {
+  max1 = parseFloat(process.argv[3]);
+  max2 = parseFloat(process.argv[2]);
 }
 
 for (let i = 4; i < process.argv.length; i++) {
-  if (process.argv[i] < max1 && process.argv[i] > max2) {
-    max2 = process.argv[i];
-  } else if (process.argv[i] > max1) {
+  if (parseFloat(process.argv[i]) < max1 && parseFloat(process.argv[i]) > max2) {
+    max2 = parseFloat(process.argv[i]);
+  } else if (parseFloat(process.argv[i]) > max1) {
     max2 = max1;
-    max1 = process.argv[i];
+    max1 = parseFloat(process.argv[i]);
   }
 }
 if (process.argv.length > 3) { console.log(max2); } else { console.log('0'); }
